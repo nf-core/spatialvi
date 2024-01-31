aec833ff3ea98906e0630f543a32b8a1a085eb9d
Christophe Avenel <christophe.avenel@it.uu.se>
2024-01-31 11:23:56 +0100

    Fix black and prettier

--- bin/read_st_data.py
+++ bin/read_st_data.py
@@ -12,14 +12,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "--SRCountDir", metavar="SRCountDir", type=str, default=None, help="Input directory with Spaceranger data."
     )
-    parser.add_argument("--output_sdata", metavar="output_sdata", type=str, default=None, help="Output spatialdata zarr path.")
-    #TODO Add argument with meta.id for dataset_id
-    
+    parser.add_argument(
+        "--output_sdata", metavar="output_sdata", type=str, default=None, help="Output spatialdata zarr path."
+    )
+    # TODO Add argument with meta.id for dataset_id
+
     args = parser.parse_args()
 
     # Read Visium data
-    st_spatialdata = spatialdata_io.visium(args.SRCountDir, counts_file ="raw_feature_bc_matrix.h5", dataset_id ="visium")
+    st_spatialdata = spatialdata_io.visium(args.SRCountDir, counts_file="raw_feature_bc_matrix.h5", dataset_id="visium")
 
     # Write raw spatialdata to file
     st_spatialdata.write(args.output_sdata, overwrite=True)
-    
